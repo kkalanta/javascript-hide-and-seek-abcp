df66f4ff11c7087e0e5380261109ea5ec58c1d09
Kimia Kalantari <kimiakalantari1356@gmail.com>
2018-01-23 14:36:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,10 +23,10 @@ function nestedTarget(){
 }*/
 
 function increaseRankBy(n) {
-  const rankedLists = document.querySelectorAll('.ranked-list')
+  var rank = document.querySelectorAll('.ranked-list')
 
-  for (let i = 0, l = rankedLists.length; i < l; i++) {
-    let children = rankedLists[i].children
+  for (let i = 0, i < rank.length; i++) {
+    let children = rank[i].children
 
     for (let j = 0, k = children.length; j < k; j++) {
       children[j].innerHTML = parseInt(children[j].innerHTML) + n
