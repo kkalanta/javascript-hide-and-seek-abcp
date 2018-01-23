7979fd120f3dd4de45dce79bc6218c1df2bdb681
Kimia Kalantari <kimiakalantari1356@gmail.com>
2018-01-23 14:28:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,8 +23,20 @@ function increaseRankBy(n){
 
 
 // Fourth function
-function deepestChild(){
-  var id = document.getElementById('grand-node')
-  var children = id.children[3]
-  return children
+//function deepestChild(){
+  //var id = document.getElementById('grand-node')
+  //var children = id.children[3]
+  //return children
+//}
+
+function deepestChild() {
+  let node = document.getElementById('grand-node')
+  let nextNode = node.children[0]
+
+  while (nextNode) {
+    node = nextNode
+    nextNode = node.children[0]
+  }
+
+  return node
 }
