91e80eb11be6e73674759d2b5365e99bef42c6d8
Kimia Kalantari <kimiakalantari1356@gmail.com>
2018-01-23 14:56:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ function increaseRankBy(n){
   var rank = document.querySelectorAll('.ranked-list')
 
   for(let i = 0; i < rank.lenght; i++){
-    let children = rank[i].children;
+    let children = rank[i].children
 
     for(let a = 0; a < children.lenght; a++){
-      let children[a].innerHTML = parsInt(children[a].innerHTML) + n;
+      let children[a].innerHTML = parsInt(children[a].innerHTML) + n
     }
 }
 
