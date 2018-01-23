d1bd5eb1ee2209e6ddd44198f11e66f8862407a2
Kimia Kalantari <kimiakalantari1356@gmail.com>
2018-01-23 13:37:57 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,29 @@
+
+function getFirstSelector(selector){
+  //const elm = document.querySelectorAll('main')
+  //const first = elm.getElementById('app')
+  var first = document.getElementById('ranked-list').querySelectorAll('ul.ranked-list li')
+  return console.log(first)
+  }
+
+
+
+// Second Function
+function nestedTarget(){
+  var divs = document.getElementByClassName('target').getElementById('nested').querySelectorAll('div');
+  //var divs2 = divs.getElementByClassName('target');
+  console.log(divs)
+}
+
+//nestedTarget();
+
+// Third function
+function increaseRankBy(n){
+
+}
+
+
+// Fourth function
+function deepestChild(){
+
+}
